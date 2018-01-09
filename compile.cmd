92b8bdc6878929c14e6110f7f17d4e9e0378e6c2
Ben <woah_its_ben@yahoo.com>
2018-01-08 23:30:27 -0600

    Add files via upload

---
+++ compile.cmd
@@ -0,0 +1,11 @@
+:: ------------------------------------------------------------------------- ::
+@echo off
+title Compile Commands
+:: ------------------------------------------------------------------------- ::
+
+echo Navigating to directory
+cd "C:\Users\Otay\Desktop\Career Files\programming\java\Latin Squares Project"
+echo Compiling project
+javac -d ./bin ./src/MagicHunter/*.java
+echo Commands completed. & echo.
+pause
---
+++ run.cmd
@@ -0,0 +1,10 @@
+:: ------------------------------------------------------------------------- ::
+@echo off
+title Run Magic Hunter
+:: ------------------------------------------------------------------------- ::
+
+echo Navigating to directory
+cd "C:\Users\Otay\Desktop\Career Files\programming\java\Latin Squares Project\bin"
+echo Running project
+java -cp . MagicHunter.Driver
+pause
